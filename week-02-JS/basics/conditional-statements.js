df050fee0ef746f3cd72788fee99bf1ec7e71c5f
owais9061 <owaischaudhry906@gmail.com>
2022-12-16 00:24:58 +0500

    Update

--- week-02-JS/basics/conditional-statements.js
+++ week-02-JS/basics/conditional-statements.js
@@ -12,3 +12,18 @@ else {
     console.log('Tell User to Login')
 }
 
+
+//EXAMPLE
+let cash = 100
+let price = 40
+let difference = cash - price
+if (cash>price)
+{
+    console.log('You have' ${difference} + " amount left")
+}
+else if ( cash==price) {
+    console.log('You have no money left')
+}
+else {
+    console.log('you are {cash - price} amount short')
+}
