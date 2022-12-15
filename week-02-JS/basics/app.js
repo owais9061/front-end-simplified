a019337b586a30be7d27f9de9950ce1e3da9a61d
owais9061 <owaischaudhry906@gmail.com>
2022-12-15 23:49:27 +0500

    Update FIle

--- week-02-JS/basics/app.js
+++ week-02-JS/basics/app.js
@@ -1,6 +1,7 @@
-// console.log('Owais Gujjar'[0]) //Print a certain index number value in the string 
-// console.log('Owais'.length)
-// let str = 'Owais'
-// console.log(str[-1])
-let str = "Owais Chaudhry"
-console.log(str[str.length -2])
+console.log('Owais Gujjar'[0]) //Print a certain index number value in the string 
+console.log('Owais'.length)
+let str = 'Owais'
+console.log(str[-1])
+console.log(str[str.length -4])
+//String printing using index number ( 0 - n )
+
