7d6588655b74b1b3b4e0fe6617fa923b68c1db8c
owais9061 <owaischaudhry906@gmail.com>
2022-12-16 23:21:19 +0500

    Updated Questions

--- week-02-JS/basics/conditional-statements.js
+++ week-02-JS/basics/conditional-statements.js
@@ -1,29 +1,65 @@
-//IF ELSE IN JAVASCRIPT
+// // IF ELSE IN JAVASCRIPT
 
-let subscribed = false
-let loggedIn = false
-if (subscribed == true){
-    console.log('Owais Chaudhry')
-}
-else if (loggedIn == true) {
-    console.log('Tell user to upgrade susbscription')
-}
-else {
-    console.log('Tell User to Login')
-}
+// let subscribedd = false
+// let loggedInn = false
+// if (subscribed == true){
+//     console.log('Owais Chaudhry')
+// }
+// else if (loggedInn == true) {
+//     console.log('Tell user to upgrade susbscription')
+// }
+// else {
+//     console.log('Tell User to Login')
+// }
 
 
-//EXAMPLE
-let cash = 100
-let price = 40
-let difference = cash - price
-if (cash>price)
-{
-    console.log('You have +  amount left')
-}
-else if ( cash==price) {
-    console.log('You have no money left')
-}
-else {
-    console.log('you are {cash - price} amount short')
-}
+// //EXAMPLE
+// let cash = 100
+// let price = 40
+// let difference = cash - price
+
+// if (cash>price)
+// {
+//     console.log(`You have amount left`)
+// }
+// else if ( cash==price) {
+//     console.log('You have no dollar')
+// }
+// else {
+//     console.log('you are {cash - price} amount short')
+// }
+
+
+// let cassh = 500;
+// let pricee = 400;
+// let storeOpen = false
+
+// if(cassh >= pricee || storeOpen == true){
+//     console.log("You have some amount left")
+// } 
+// else{
+//     console.log("You are completely broke")
+// }
+
+
+// // Ternary Operator
+// let hot = false
+// // putting true or false after hot basically activates or deactivate 
+// // the first term bfore ( : ) ternary operator comparator
+// hot?console.log('Whether is Hot'):console.log('Weatheris not hot')
+
+// let subscribed = false
+// let loggedIn = true
+// let str = subscribed || loggedIn?'Show the video':'Hide the video'
+// console.log(str);
+
+
+// let cashh = 50
+// let priice = 40
+// let storeIsOpen = true
+// create code to give reciept or to not
+// method 1
+// cashh > priice && storeIsOpen?console.log('Print Reciept'):console.log('Donto print')
+// method 2
+// let strr = cashh>priice && storeIsOpen?"Print Voucher":"Donot print voucher"
+// console.log(strr);
