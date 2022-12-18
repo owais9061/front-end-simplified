6c182d3aef1a4c517d9c023b2498f212eb1e12de
owais9061 <owaischaudhry906@gmail.com>
2022-12-18 22:54:38 +0500

    ARRAYS IN JS

--- week-02-JS/basics/arrays.js
+++ week-02-JS/basics/arrays.js
@@ -9,3 +9,33 @@ console.log(item1, item2,item3,item4,item5)
 //ANOTHER METHOD IS TO USE ARRAY
 let arr = [10, 20, 30, 40,50]
 console.log("Prices of items are:", arr);
+
+//PRINT SPECIFIC VALUE
+console.log(arr[0]); //change 0 to any number
+
+//Print the length of array
+console.log(arr(arr[arr.length]));
+
+
+//Understand the .push command of JS for arrays
+//Use to insert values in array
+//Think it as a shopping cart
+
+//STEP 1 ----> Cart is empty
+//so
+let cart = []
+
+//STEP 2-----> if i click on add to cart below a shirt then
+cart.push('shirt')
+
+//STEP 3 -----> Now the cart has Shirt listing
+console.log(cart);
+
+
+
+
+
+
+//ARRAY FILTER METHOD
+let arr1 = [10,20,30,40,'Owais']
+arr1.filter( ()=>{} )
