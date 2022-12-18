668b7240cf161ab3cc694e5688692972016c76d6
owais9061 <owaischaudhry906@gmail.com>
2022-12-18 22:18:09 +0500

    Updated Functions

--- week-02-JS/basics/functions.js
+++ week-02-JS/basics/functions.js
@@ -31,3 +31,31 @@ anotherPracticeQuestions('Owais', 'Chaudhry');
 anotherPracticeQuestions('Owais', 'Gujjar');
 anotherPracticeQuestions('Owais', 'Khan');
 anotherPracticeQuestions('The Foolish', 'Engineer');
+
+
+//Return statement in functions
+function fn() {
+    return 6 +10
+    console.log('Owais khan')
+}
+console.log(fn()); //output 16
+//OR
+console.log(5); //output 6
+//this code will generate the output " 5" not the console.log value
+//becuase tje fnction get terminated at tje return statement
+
+
+//creating another type of function
+function sumOfTwo(num1, num2) {
+    return 5 + 10
+}
+console.log(sumOfTwo(5, 10));
+
+
+function sumOfTwoNumbers(num1, num2) {
+    return num1 + num2
+}
+console.log(sumOfTwoNumbers(5, 10)); 
+//it will now give output 15 becuase the function has it's repeatable values which is num1 and num2
+// and it will just ask for the values which are changebale like 5 or 10
+
