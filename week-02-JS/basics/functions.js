8ffb77e3900293ef82ac244c04b44dc3c70ffb8a
owais9061 <owaischaudhry906@gmail.com>
2022-12-18 22:05:27 +0500

    Updated Function JS Files

---
+++ week-02-JS/basics/functions.js
@@ -0,0 +1,22 @@
+//practicng Functions
+const str = "Owais Gujjar"
+function justPracticeQuestions() {
+    for (str = 0; str<10; str++) {
+        console.log(str);
+    }
+}
+// this function will print Owais Gujjar 0- times
+justPracticeQuestions();
+
+//another type of functions
+//user input based
+
+function anotherPracticeQuestions (name) {
+    console.log(`welcome to House, ${name}`);
+}
+//Call the function
+//in the braces we will write what we need to print everytime after the keyword welcome to house
+anotherPracticeQuestions('Owais');
+anotherPracticeQuestions('Gujjar');
+anotherPracticeQuestions('Owais');
+anotherPracticeQuestions('Chaudhry');
