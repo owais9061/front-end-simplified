a4bad16e33336d28d2fdea59eeccfcbc8e2be5b5
owais9061 <owaischaudhry906@gmail.com>
2022-12-18 22:45:02 +0500

    Arrays Explanation

---
+++ week-02-JS/basics/arrays.js
@@ -0,0 +1,11 @@
+//Creating 5 variables
+let item1 = 10 
+let item2 = 20 
+let item3 = 30 
+let item4 = 40 
+let item5 = 50 
+console.log(item1, item2,item3,item4,item5)
+
+//ANOTHER METHOD IS TO USE ARRAY
+let arr = [10, 20, 30, 40,50]
+console.log("Prices of items are:", arr);
