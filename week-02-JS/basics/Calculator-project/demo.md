91df73f08276368a1e268d02ab07c47168371344
owais9061 <owaischaudhry906@gmail.com>
2022-12-30 00:53:56 +0500

    Price card

---
+++ week-02-JS/basics/Calculator-project/demo.md
@@ -0,0 +1,15 @@
+---
+marp: true;
+---
+
+# An Intro to Markdown
+
+By: Owais Chaudhry
+
+---
+
+## Slide Header
+
+* Use Markdown to create slides.
+* Use Markdown to create deck.
+* Go f*ck yourself.
--- week-02-JS/basics/index.js
+++ week-02-JS/basics/index.js
@@ -3,3 +3,8 @@ let scope = "food" + "bar";
 console.log(scope);
 let lowerOwaisChaudhry = "Owais Chaudhry".toLowerCase();
 console.log(lowerOwaisChaudhry);
+
+//INCLUDE METHOD TO CHECK THE AVAILIBILITY OF STRINGS
+console.log(lowerOwaisChaudhry.includes('P'));
+//Also use indexation method
+console.log(lowerOwaisChaudhry.includes('Owais',0));
