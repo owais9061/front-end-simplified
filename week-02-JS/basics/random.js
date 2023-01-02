d09479c034e7959409dbecc44fabb7b45815db20
owais9061 <owaischaudhry906@gmail.com>
2023-01-02 07:53:57 +0500

    Update

---
+++ week-02-JS/basics/random.js
@@ -0,0 +1,4 @@
+let daysOfWeek = ["MONDAY" , "TUESDAY" , "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY" , "SUNDAY"]
+let now = new Date();
+let dayName = daysOfWeek[now.getDay()];
+alert("LOL It's  " + dayName + ".");
