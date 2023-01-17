6dc37a0d89c0f257341c7d3b20741d512b582e1f
owais9061 <owaischaudhry906@gmail.com>
2023-01-17 22:03:06 +0500

    Additon of Strings

---
+++ week-02-JS/basics-02/Data-Types.js
@@ -0,0 +1,10 @@
+// Sring
+console.log('Owais Gujjar')
+console.log("Owais Gujjar")
+
+// Concatenation
+console.log('Owais ' + 'Gujjar')
+console.log('Owais + Gujjar')
+console.log("100") //Numbers
+console.log('Owais'[0])
+console.log('Owais Gujjar'.length)
