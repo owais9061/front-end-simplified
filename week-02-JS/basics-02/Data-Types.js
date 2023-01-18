45d736988d03552835948ab08390b2e97785eacf
owais9061 <owaischaudhry906@gmail.com>
2023-01-18 21:44:54 +0500

    Additon of Conditional Statements File

--- week-02-JS/basics-02/Data-Types.js
+++ week-02-JS/basics-02/Data-Types.js
@@ -1,10 +1,51 @@
-// Sring
-console.log('Owais Gujjar')
-console.log("Owais Gujjar")
+// // Sring
+// console.log('Owais Gujjar')
+// console.log("Owais Gujjar")
 
-// Concatenation
-console.log('Owais ' + 'Gujjar')
-console.log('Owais + Gujjar')
-console.log("100") //Numbers
-console.log('Owais'[0])
-console.log('Owais Gujjar'.length)
+// // Concatenation
+// console.log('Owais ' + 'Gujjar')
+// console.log('Owais + Gujjar')
+// console.log("100") //Numbers
+// console.log('Owais'[0])
+// console.log('Owais Gujjar'.length)
+
+
+let str = 'Canny Coders'
+console.log(str[4])
+// whole numbers 0--> 1,2,3
+// Negative Whole -1,-2,-3,-4
+console.log(str[str.length-3])
+
+// Data Types
+
+//VARIABLES
+
+const isRaining = true
+const hotWather = false
+
+let fullName = 'Owais'
+fullName = fullName + ' Gujjar'
+console.log(fullName)
+
+//PROGRAM #01
+//Convert Celcius to Farenhiet
+
+let celcius = 20
+let Farenhiet = (celcius*1.8) +32
+console.log(Farenhiet)
+
+
+let bool = '1' === 1
+console.log(bool)
+
+
+
+
+
+
+
+
+
+
+
+
