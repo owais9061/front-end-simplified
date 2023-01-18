13bf0ce7e87f60e050b08ddb9aceea91d74347e3
owais9061 <owaischaudhry906@gmail.com>
2023-01-18 23:45:36 +0500

    Loops Addition

---
+++ week-02-JS/basics-02/loops.js
@@ -0,0 +1,50 @@
+// let count = 0
+// while (count<3) {
+//     console.log(count)
+//     count=count+1;
+// }
+
+// for ( let i=0; i<=; i++) {
+//     console.log(i);
+// }
+
+//! Write a for loop for values from 1 to 20
+//? if ( i ) is divisible by 3, print (canny)
+//? if ( i ) is divisible by 5, print (coders)
+//? if ( i ) is divisible by 3 & 5, print (canny coders)
+//? if ( i ) is not divisible by 3 or 5, print (numbers)
+
+// for(let i=0; i<20; i++){
+//     if ( i%3 == 0 ) {
+//         console.log(`${i} --> Canny`);
+//     }
+//     else if ( i%5 == 0 ) {
+//         console.log(`${i} --> Coders`);
+//     }
+//     else if ( i%3 || i%5 == 0 ) {
+//         console.log(`${i} --> Canny Coders`);
+//     }
+//     else {
+//         console.log(`${i} --> ${i}`)
+//     }
+// }
+
+
+//!print out every single character from a string
+//! ' Couch Coding '
+let str = 'Couch Coding'
+for (let i = 0; i < str.length; i++) {
+    console.log(str[i]); 
+}
+
+
+
+
+
+
+
+
+
+
+
+
