1e7e5b95a867d642043edd41dca58926d587c044
owais9061 <owaischaudhry906@gmail.com>
2023-01-18 22:36:46 +0500

    FInalized Conditonals

---
+++ week-02-JS/basics-02/conditional-statements.js
@@ -0,0 +1,71 @@
+// let subscribed = false
+// if (subscribed = false) {
+//     console.log('Subscriber')
+// }
+// else {
+//     console.log('Not a Subscriber')
+// }
+
+
+// also!
+// let subscribed = false
+// let loggedIn = true
+// if (subscribed != true){console.log('Subscriber')}
+// else if (loggedIn=true){console.log('Not a Subscriber')}
+// else{console.log('Error')}
+
+// // PROBLEM #03
+// //STORE
+// let cash = 100;
+// let price = 60;
+// let difference = cash - price;
+
+// if(cash>price)
+// {
+//     console.log('You paid some extra amount ' + difference + ' rupees left')
+// }
+// else if( cash == price ) {
+//     console.log("You left no balanace")
+// }
+// else {
+//     console.log('You are out of money')
+// }
+
+
+// let cash = 50;
+// let price = 40;
+// let storeIsOpen = true;
+// if( cash>=price &&  storeIsOpen !== true ) {
+//     console.log('Print the Reciept')
+// }
+
+// let subscribed = true;
+// let loggedIn = false;
+// let str = subscribed && loggedIn ? 'Owais':'Raza'
+// console.log(str)
+
+// let cash = 50
+// let price =40
+// let storeIsOpen = true
+// let str = cash>price && storeIsOpen !== false ? 'give reciept':'donot print'
+// console.log(str)
+
+//Ternary
+let hot = true
+hot ? console.log('Weatther is Hot'):console.log('Weather is not Hot')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
