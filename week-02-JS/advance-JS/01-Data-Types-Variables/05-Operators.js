f1cd730d6845694c581ece9b9eac18e05e787038
owais9061 <owaischaudhry906@gmail.com>
2023-09-05 17:10:37 +0500

    Done with Data Types & Operators, Now working on Strings

---
+++ week-02-JS/advance-JS/01-Data-Types-Variables/05-Operators.js
@@ -0,0 +1,11 @@
+/**
+ * TYPES OF OPERATORS:
+ * 1- Artihmetic (+, -, /, *, % (Modulus) ),
+ * 2- Concatenation: 'Owais' + 'Gujjar'
+ * 3- Exponent (**) : 2**5 = 32
+ * 4- Increment ++ or += / Decrement -- or -=
+ * 5- Assignemnt: = , +=, -=
+ * 6- Comparison: (>, <, <=, >=, !, !=)
+ * NOTE: == equates the value while === euqates the Data Types & Values: x=2 === x='2'
+ * 7- 
+*/
